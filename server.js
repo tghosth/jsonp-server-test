faa7e881e266e656b71c2f36e6a59bc3e89c53a0
Josh Grossman <tghosth@users.noreply.github.com>
2018-03-18 21:51:25 +0200

    Expand the main serverside with more methods with different characteristics

--- server.js
+++ server.js
@@ -1,22 +1,51 @@
 'use strict';
 
 var express = require('express');
+var cookieParser = require('cookie-parser')
+const basicAuth = require('express-basic-auth')
 
 
-// var app = module.exports = express.createServer();
 var app = module.exports = express();
+app.use(cookieParser())
+
 // this enables jsonp support
 app.set("jsonp callback name", "cb");
 
+app.get('/dee', function(req, res){ 
+  
+  var nocookie = false
+  
+  if (req.cookies === undefined)
+  {
+    nocookie = true;
+  }
+  else if (req.cookies['blah'] === undefined)
+  {
+    nocookie = true;
+  }
+
+  if (nocookie)
+  {
+    res.cookie("blah", "fred",{})
+    res.json("NO COOKIE DETECTED!!!!");
+  }
+  else
+  {
+    res.jsonp("super secret response data dee");
+  }
+});
 
 app.get('/foo', function(req, res){ 
-  // important - you have to use the response.json method
   res.jsonp("super secret response data foo");
 });
 
+app.use(basicAuth({
+  users: { 'admin': 'supersecret' }, 
+  challenge: true
+}))
+
 app.get('/bar', function(req, res){ 
-  // important - you have to use the response.json method
-  res.jsonp("super secret response data bar");
+    res.jsonp("super secret response data bar");
 });
 
 var listener = app.listen(3000, function(){
